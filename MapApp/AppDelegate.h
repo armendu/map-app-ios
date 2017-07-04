90c0ab30d9fab0b12c50f851b9d1e6e488fbefd2
Armend Ukëhaxhaj <a.ukehaxhaj@abidat.de port = 19022>
2017-07-04 17:15:24 +0200

    Initial commit

---
+++ MapApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapApp
+//
+//  Created by Armend Ukehaxhaj on 7/2/17.
+//  Copyright © 2017 Armend Ukehaxhaj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
